61269b9ab7b03a4d18bd5ea180dd510d18e56053
Andrew Seymour <andrewseymour@Andrews-MacBook-Pro-2.local>
2022-06-02 15:41:11 -0500

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,26 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+
+    // create div to hold task infor and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
+
+    // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     listItemEl.appendChild(taskInfoEl);
+
+    // add entire item to list
     tasksToDoEl.appendChild(listItemEl);
-    console.log(taskTypeInput);
-    console.dir(taskNameInput);
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = taskNameInput;
+ 
+    
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
